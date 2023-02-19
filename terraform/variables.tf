1420b847d60e58bd4e9b74fe7051c575db0d8b0a
Gletion Campos <gleiton.ti@gmail.com>
2023-02-19 18:59:31 -0300

    Creating IaC Files

---
+++ terraform/variables.tf
@@ -0,0 +1,25 @@
+variable "region" {
+  type        = string
+  description = "The AWS Region to use"
+  default     = env.AWS_DEFAULT_REGION
+}
+variable "bucket_prefix" {
+  type        = string
+  description = "The prefix for the S3 bucket"
+  default     = "tf-s3-website-"
+}
+variable "domain_name" {
+  type        = string
+  description = "The domain name to use"
+  default     = "calculator.gleitoncampos.cf"
+}
+variable "service_name" {
+  type        = string
+  description = "The service name"
+  default     = "calculator-example"
+}
+variable "build_folder" {
+  type        = string
+  description = "The folder that build artifacts are available to upload do s3"
+  default     = "calculator/build/"
+}
