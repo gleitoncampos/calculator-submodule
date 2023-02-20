8e302e3c4fb160d27daf6b3cc6c5997d09b47e78
Gletion Campos <gleiton.ti@gmail.com>
2023-02-20 16:50:52 -0300

    .

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set +x
 echo "Listing worksapce"
-cd /app/calculator || return 1
+cd ./calculator || return 1
+echo "$(pwd)"
 echo "$(ls -lah)"
 echo "Building using docker"
 docker run  -v ${{ github.workspace }}:/app node:15 /bin/bash -c "npm install && yarn build"
